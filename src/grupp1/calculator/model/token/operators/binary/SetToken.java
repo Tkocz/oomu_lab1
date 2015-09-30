a4f2776f4238b135d3dc7580f47c3103d2767b9c
Philip Arvidsson <philiparvidsson@users.noreply.github.com>
2015-09-30 22:15:40 +0200

    Stuff.

--- src/grupp1/calculator/model/token/operators/binary/SetToken.java
+++ src/grupp1/calculator/model/token/operators/binary/SetToken.java
@@ -1,5 +1,26 @@
 package grupp1.calculator.model.token.operators.binary;
 
+/*
+Ni vill undvika att använda "instanceof" i eran kod, om möjligt (ni använder
+detta i SetToken). Detta är oftast en tecken på att den objektorienterade
+designen behöver ses över.
+
+KOMMENTAR:
+
+    Jag förstår inte kritiken. Då parametern stack bara är en stack med bas-
+    klass token så kan vi inte garantera typen av nästkommande token. Skriver
+    man ex. '1 =' så kommer vi få en OperandToken efter SetToken eftersom dessa
+    skapas i TokenFactory. Det finns inget sätt att komma runt detta med hjälp
+    av objektorienterade paradigmer - mig veterligen. Ett sätt är möjligtvis att
+    ha någon form av kontext när tokens instansieras i TokenFactory, men detta
+    vore i sig ett brott mot OO-principer. Ytterligare en tänkbar lösning är att
+    bygga ett syntaxträd och därutöver en lexer som verifierar att syntaxen är
+    korrekt. Detta är överkurs gånger hundra för labbens ändamål. instanceof är
+    en adekvat lösning.
+
+    // Philip
+*/
+
 import grupp1.calculator.exceptions.InvalidOperationException;
 import grupp1.calculator.model.token.OperatorInfo;
 import grupp1.calculator.model.token.OperatorToken;
@@ -32,14 +53,14 @@ public SetToken(String op) {
 @Override
 public double eval(Stack<Token> stack) throws Exception {
     Token token = stack.pop();
-    
+
     if (!(token instanceof VarToken))
-        throw new InvalidOperationException();
-    
+        throw new InvalidOperationException("Expected variable name.");
+
     double val = stack.pop().eval(stack);
-    
+
     VarToken.setVar(token.GetString(), val);
-    
+
     return (val);
 }
 
